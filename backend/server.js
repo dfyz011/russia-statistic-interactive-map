f747bcc5dbd9258734c6deb6b0c6f0a21d482418
Baynov Ivan <Jhon0@yandex.ru>
2020-06-15 14:28:02 +0500

    changed ports

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const appRouter = require('./routes/index');
 
 
 app.use('/api', appRouter);
-const port = parseInt(process.env.PORT, 10) || 8000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port);
