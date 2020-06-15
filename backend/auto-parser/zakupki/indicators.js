465c52d0f7b526a4872531eec3b7d92677886fc7
Baynov Ivan <Jhon0@yandex.ru>
2020-06-15 20:31:22 +0500

    added sanbox param to puppeter

--- backend/auto-parser/zakupki/indicators.js
+++ backend/auto-parser/zakupki/indicators.js
@@ -1,7 +1,13 @@
 const puppeteer = require('puppeteer');
 
 const parseIndicators = async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      'no-sandbox',
+      'disable-setuid-sandbox',
+    ]
+  });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.goto('https://zakupki.gov.ru/epz/main/public/home.html', { waitUntil: 'domcontentloaded' });
