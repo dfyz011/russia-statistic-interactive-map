30828d0343ad0032d8c2f3c9e898a4d43921b26b
Baynov Ivan <Jhon0@yandex.ru>
2020-06-20 22:29:01 +0500

    added build folder to project, because i dont have enough memory to build project on server

---
+++ client/build/precache-manifest.01b44ab9c188795e0330e1dae809c8f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1435a7124332e6a7ef07b3e4593b4c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73ff9941bd94b6b86558",
+    "url": "/static/css/main.e9e324b5.chunk.css"
+  },
+  {
+    "revision": "7da765f59ce2dec22d02",
+    "url": "/static/js/2.fc114c1c.chunk.js"
+  },
+  {
+    "revision": "2814935d429327a7caee6d41e62fdb39",
+    "url": "/static/js/2.fc114c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ff9941bd94b6b86558",
+    "url": "/static/js/main.484b4170.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
